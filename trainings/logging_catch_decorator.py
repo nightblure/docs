052791dfdfcf1235ef8a45b0d530aef0e7b9c270
Ivan Belyaev <vanobel159@gmail.com>
2024-02-12 16:47:46 +0300

    Update logging_catch_decorator.py

--- trainings/logging_catch_decorator.py
+++ trainings/logging_catch_decorator.py
@@ -1,12 +1,10 @@
 import logging
-from datetime import datetime
 from functools import wraps
 
+logger = logging.getLogger(__name__)
 
-logger = logging.getLogger()
 
-
-def catch_and_log():
+def log_if_errors(*, reraise=True):
     def wrapper(f):
         @wraps(f)
         def inner(*a, **kw):
@@ -15,31 +13,10 @@ def catch_and_log():
             except Exception as e:
                 f_name = f.__qualname__
                 logger.error(f'Error at "{f_name}": {str(e)}')
-                logger.error('Calling signature with passed parameters:')
-                logger.error(f'{f_name}(')
-                
-                for arg in a:
-                    logger.error(f'    {arg},')
-                
-                for arg, value in kw.items():
-                    logger.error(f'    {arg}={str(value)},')
-                
-                logger.error(')')
-                
-                if a:
-                    logger.error(f'args: {a}')
-                if kw:
-                    logger.error(f'kwargs:')
-                    for arg, value in kw.items():
-                        logger.error(f'{arg}: {value}')
+
+                if reraise:
+                    raise e
+
         return inner
+
     return wrapper
-
-class Test:
-    @staticmethod
-    @catch_and_log()
-    def test(a, b, *, c=None, d=3):
-        x = 1 / 0
-
-
-Test.test(5, 'sef', c=type, d=datetime.now())
